e0aa784c90b7a45b2c57a4d5ef1ee270443b3897
libetl <libe4@free.fr>
2015-02-10 00:31:30 +0100

    some work but unfortunately not able to run something yet

--- src/org/toilelibre/libe/singin/Sound2GraphSeries.java
+++ src/org/toilelibre/libe/singin/Sound2GraphSeries.java
@@ -7,7 +7,16 @@ import com.jjoe64.graphview.series.LineGraphSeries;
 
 public class Sound2GraphSeries {
 
-    public LineGraphSeries<DataPoint> convert (Sound s){
-        return null;
+    public LineGraphSeries<DataPoint> convert (Sound sound){
+        DataPoint [] dataPointArray = new DataPoint [sound.getSamples ().length];
+        long neutral = this.getNeutral (sound.getNbBytesPerSample ());
+        for (int i = 0 ; i < dataPointArray.length ; i++){
+            dataPointArray [i] = new DataPoint (i, sound.getSamples () [i] - neutral);
+        }
+        return new LineGraphSeries<DataPoint> (dataPointArray);
+    }
+    
+    public long getNeutral (int nbBytesPerSample){
+        return (long)Math.pow (256, nbBytesPerSample) / 2;
     }
 }
--- src/org/toilelibre/libe/singin/SoundFragment.java
+++ src/org/toilelibre/libe/singin/SoundFragment.java
@@ -1,52 +1,38 @@
 package org.toilelibre.libe.singin;
 
-import com.jjoe64.graphview.GraphView;
-import com.jjoe64.graphview.series.DataPoint;
-import com.jjoe64.graphview.series.PointsGraphSeries;
+import java.io.File;
+
+import org.toilelibre.libe.soundtransform.actions.fluent.FluentClient;
+import org.toilelibre.libe.soundtransform.model.converted.sound.Sound;
+import org.toilelibre.libe.soundtransform.model.exception.SoundTransformException;
 
 import android.app.Activity;
 import android.app.Fragment;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
 
 public class SoundFragment extends Fragment {
 
     @Override
     public View onCreateView (LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        
+
         View rootView = inflater.inflate (R.layout.channels, container, false);
-        GraphView graph = (GraphView) rootView.findViewById (R.id.graph1);
-        PointsGraphSeries<DataPoint> series = new PointsGraphSeries<DataPoint> (new DataPoint [] { new DataPoint (0, -2), new DataPoint (1, 5), new DataPoint (2, 3), new DataPoint (3, 2),
-                new DataPoint (4, 6) });
-        graph.addSeries (series);
-        series.setShape (PointsGraphSeries.Shape.POINT);
-        PointsGraphSeries<DataPoint> series2 = new PointsGraphSeries<DataPoint> (new DataPoint [] { new DataPoint (0, -1), new DataPoint (1, 4), new DataPoint (2, 2), new DataPoint (3, 1),
-                new DataPoint (4, 5) });
-        graph.addSeries (series2);
-        series2.setShape (PointsGraphSeries.Shape.RECTANGLE);
-        series2.setColor (Color.RED);
-        PointsGraphSeries<DataPoint> series3 = new PointsGraphSeries<DataPoint> (new DataPoint [] { new DataPoint (0, 0), new DataPoint (1, 3), new DataPoint (2, 1), new DataPoint (3, 0),
-                new DataPoint (4, 4) });
-        graph.addSeries (series3);
-        series3.setShape (PointsGraphSeries.Shape.TRIANGLE);
-        series3.setColor (Color.YELLOW);
-        PointsGraphSeries<DataPoint> series4 = new PointsGraphSeries<DataPoint> (new DataPoint [] { new DataPoint (0, 1), new DataPoint (1, 2), new DataPoint (2, 0), new DataPoint (3, -1),
-                new DataPoint (4, 3) });
-        graph.addSeries (series4);
-        series4.setColor (Color.GREEN);
-        series4.setCustomShape (new PointsGraphSeries.CustomShape () {
-            @Override
-            public void draw (Canvas canvas, Paint paint, float x, float y) {
-                paint.setStrokeWidth (10);
-                canvas.drawLine (x - 20, y - 20, x + 20, y + 20, paint);
-                canvas.drawLine (x + 20, y - 20, x - 20, y + 20, paint);
-            }
-        });
+        try {
+            Sound [] sounds = FluentClient.start ().withFile (new File (Environment.getExternalStorageDirectory().getPath() + "/before.wav")).convertIntoSound ().stopWithSounds ();
+            GraphView graph = (GraphView) rootView.findViewById (R.id.graph1);
+            LineGraphSeries<DataPoint> series = new Sound2GraphSeries ().convert (sounds [0]);
+            graph.addSeries (series);
+            series.setThickness (2);
+        } catch (SoundTransformException e) {
+            e.printStackTrace ();
+        }
         return rootView;
     }
 
